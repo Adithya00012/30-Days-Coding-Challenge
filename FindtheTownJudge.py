de74fba8686331cf79e4062690ad679f89e1a02a
Adithya00012 <k.adithyaadithya1432@gmail.com>
2025-09-14 23:33:45 +0530

    Create FindtheTownJudge.py

---
+++ FindtheTownJudge.py
@@ -0,0 +1,16 @@
+def find_judge(n, trust):
+  if n == 1:
+      return 1
+
+  in_degree = [0] * (n + 1)
+  out_degree = [0] * (n + 1)
+
+  for a, b in trust:
+      out_degree[a] += 1
+      in_degree[b] += 1
+
+  for i in range(1, n + 1):
+      if out_degree[i] == 0 and in_degree[i] == n - 1:
+          return i
+
+  return -1
